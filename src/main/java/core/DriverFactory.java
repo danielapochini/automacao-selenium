496c2aef2759ff2383f8399c5e7e0ba8a6ecb3df
danielapochini <daniela.pochini@fatec.sp.gov.br>
2020-09-25 12:12:18 -0300

    Chaveamento de Browsers

--- src/main/java/core/DriverFactory.java
+++ src/main/java/core/DriverFactory.java
@@ -1,6 +1,7 @@
 package core;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
@@ -11,7 +12,10 @@ public class DriverFactory {
 	
 	public static WebDriver getDriver() {
 		if (driver == null) {
-			driver = new FirefoxDriver();
+			switch(Propriedades.browser) {
+			case FIREFOX: driver = new FirefoxDriver(); break;
+			case CHROME: driver = new ChromeDriver(); break;
+			} 
 			driver.manage().window().maximize(); 
 		}
 		return driver;	//retorna a mesma instancia de driver criada anteriormente
--- src/main/java/core/Propriedades.java
+++ src/main/java/core/Propriedades.java
@@ -3,4 +3,11 @@ package core;
 public class Propriedades {
 
 	public static boolean FECHAR_BROWSER = false;
+	
+	public static Browsers browser = Browsers.FIREFOX;
+	
+	public enum Browsers{
+		CHROME,
+		FIREFOX
+	}
 }
